df5ae34350b258dd56ced5acb5f809c26fb66fbd
pedroaz <pedro.azvm@gmail.com>
2021-01-24 14:36:04 -0300

    Before adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
+import { PortalPageComponent } from './Components/portal-page/portal-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LandingPageComponent, PortalPageComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
