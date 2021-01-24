92efa7ce0d932cdc1b504a9160fe53edac27e1ce
pedroaz <pedro.azvm@gmail.com>
2021-01-24 14:50:03 -0300

    App routing working!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { LandingPageComponent } from './Components/landing-page/landing-page.component';
 import { PortalPageComponent } from './Components/portal-page/portal-page.component';
+import { AppRoutingModule } from './app-routing/app-routing.module'
 
 @NgModule({
   declarations: [AppComponent, LandingPageComponent, PortalPageComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
