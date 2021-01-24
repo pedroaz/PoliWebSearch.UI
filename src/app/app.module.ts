a6c3122a2f76af00c82d2cbb5f05625657b95bf6
pedroaz <pedro.azvm@gmail.com>
2021-01-24 15:35:55 -0300

    Add UI framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from "@angular/platform-browser";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { LandingPageComponent } from './Components/landing-page/landing-page.component';
 import { PortalPageComponent } from './Components/portal-page/portal-page.component';
 import { AppRoutingModule } from './app-routing/app-routing.module'
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [AppComponent, LandingPageComponent, PortalPageComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule
   ],
   bootstrap: [AppComponent]
 })
