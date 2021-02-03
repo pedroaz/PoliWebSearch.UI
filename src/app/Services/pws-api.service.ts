2e24ec8504d90503d9a06884102e51a07b849b4e
pedroaz <pedro.azvm@gmail.com>
2021-02-02 21:03:59 -0300

    Maybe environments are working? No idea

--- src/app/Services/pws-api.service.ts
+++ src/app/Services/pws-api.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'environment/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PwsApiService {
 
-  url = "api/GetPersonInformationByCpf/04460838877"
+  // url = "api/GetPersonInformationByCpf/04460838877"
+  url = environment.baseUrl + "api/GetPersonInformationByCpf/04460838877"
 
   constructor(private httpClient: HttpClient) { }
 
